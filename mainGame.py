8349d08287db2b9ac697030fdc918c9c91560a12
Gabriel Maia Ramos <gamaia@id.uff.br>
2025-10-20 01:49:23 -0300

    alguns sprites

--- mainGame.py
+++ mainGame.py
@@ -1,13 +1,20 @@
 from pplay.window import *
+from pplay.sprite import *
 
 #=====[JANELA]=====
-largura = 600
-altura = 800
+largura = 1280
+altura = 720
 janela = Window(largura, altura)
-janela.set_background_color((0,0,80))
+
 
 #=====[Sprites]=====
+player = Sprite("C:\\Users\\Computador\\Documents\\GitHub\\gameLab\\spirtes\\00-final-product.png")
+player.set_position(largura/2, altura/2)
 
+salao_jantar = Sprite("C:\\Users\\Computador\\Documents\\GitHub\\gameLab\spirtes\\fundo_salao_jantar.png")
+
+mesa_jantar = Sprite("spirtes\mesa_jantar.png")
+mesa_jantar.set_position(largura/2, altura/2)
 #=====[LOOP]=====
 while True:
 
@@ -16,4 +23,7 @@ while True:
 
 
     #=====[upadte]=====
+    salao_jantar.draw()
+    mesa_jantar.draw()
+    player.draw()
     janela.update()
