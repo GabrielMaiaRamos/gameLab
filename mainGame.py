49b1e7ac1b0a535e205fe7d321e452898783353b
Gabriel Maia Ramos <gamaia@id.uff.br>
2025-10-20 00:11:22 -0300

    inicio de um sonho

---
+++ mainGame.py
@@ -0,0 +1,19 @@
+from pplay.window import *
+
+#=====[JANELA]=====
+largura = 600
+altura = 800
+janela = Window(largura, altura)
+janela.set_background_color((0,0,80))
+
+#=====[Sprites]=====
+
+#=====[LOOP]=====
+while True:
+
+
+    #=====[draws]=====
+
+
+    #=====[upadte]=====
+    janela.update()
