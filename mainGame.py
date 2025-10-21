07ae27deb63ba9c736ec036fda7b45cd1885cca5
Gabriel Maia Ramos <gamaia@id.uff.br>
2025-10-21 12:14:02 -0300

    comitasdasd

--- mainGame.py
+++ mainGame.py
@@ -12,6 +12,13 @@ janela = Window(largura, altura)
 player = Sprite("sprites\\player.png")
 player.set_position(largura/2 + 200, altura/2)
 
+<<<<<<< Updated upstream
+=======
+hitbox_player = Sprite("sprites\\hitbox_player.png")
+hitbox_player.set_position(largura/2 + 208, altura/2 + 135)
+
+#=====[sala de jantar]=====
+>>>>>>> Stashed changes
 salao_jantar = Sprite("sprites\\fundo_salao_jantar.png")
 
 quadro = Sprite("sprites\\quadro.png")
@@ -21,6 +28,7 @@ lareira = Sprite("sprites\\lareira.png")
 lareira.set_position(0, 0)
 
 armario = Sprite("sprites\\armario.png")
+<<<<<<< Updated upstream
 armario.set_position(1080, 200)
 
 mesa_jantar = Sprite("sprites\\mesa_jantar.png")
@@ -28,13 +36,37 @@ mesa_jantar.set_position(largura/2 - 100, altura/2)
 
 tv = Sprite("sprites\\tv.png")
 tv.set_position(950, 30)
+=======
+armario.set_position(1080, 75)
+
+mesa_jantar = Sprite("sprites\\mesa_jantar.png")
+mesa_jantar.set_position(largura/2 - 100, altura/2)
+
+bussula = Sprite("sprites\\bussula.png")
+bussula.set_position(560, 375)
+
+tv = Sprite("sprites\\tv.png")
+tv.set_position(800, 30)
+
+porta = Sprite("sprites\\porta.png")
+porta.set_position(540, 20)
+
+carrinho = Sprite("sprites\\carrinho.png")
+carrinho.set_position(1050, 500)
+
+retrato = Sprite("sprites\\retrato.png")
+retrato.set_position(300, 300)
+
+objetos_jantar = [lareira, armario, mesa_jantar]
+>>>>>>> Stashed changes
 
 #=====[Modularização]=====
 from playerMoviment import Movimentacao
-move_player = Movimentacao(player, janela)
+move_player = Movimentacao(player, hitbox_player, janela)
 
 #=====[LOOP]=====
 while True:
+<<<<<<< Updated upstream
     print(player.x, janela.height, player.x+100)
     move_player.moviment()
 
@@ -52,5 +84,36 @@ while True:
     mesa_jantar.draw()
 
     #=====[upadte]=====
+=======
+    #print(player.x, janela.height, player.x+100)
+    move_player.moviment(objetos_jantar)
+    match fase:
+        case "menu":
+            fase = "jantar"
+            pass
+        case "jantar":
+    #=====[draws]=====
+
+            print(player.x, janela.height, player.x+100)
+            move_player.moviment(objetos_jantar)
+
+            #===abaixo do player===
+            salao_jantar.draw()
+            quadro.draw()
+            armario.draw()
+            lareira.draw()
+            retrato.draw()
+            tv.draw()
+            porta.draw()
+            mesa_jantar.draw()
+            bussula.draw()
+            #===layer do player===
+            player.draw()
+            #===acima do player
+            carrinho.draw()
+
+            hitbox_player.draw()
+            #=====[upadte]=====
+>>>>>>> Stashed changes
 
     janela.update()
--- playerMoviment.py
+++ playerMoviment.py
@@ -3,14 +3,21 @@ from pplay.sprite import *
 from pplay.keyboard import *
 
 class Movimentacao():
+<<<<<<< Updated upstream
     def __init__(self, player, janela):
         self.speedUP = 100
         self.speedDOWN = 100
         self.speedRIGHT = 100
         self.speedLEFT = 100
+=======
+    def __init__(self, player, hitbox, janela):
+        self.speed = 100
+>>>>>>> Stashed changes
         self.player = player
+        self.hitbox = hitbox
         self.janela = janela
 
+<<<<<<< Updated upstream
     def moviment(self):
         if (Window.keyboard.key_pressed("w") or Window.keyboard.key_pressed("UP")) and (self.player.y+100 > 190):
             self.player.y -= self.speedUP * self.janela.delta_time()
@@ -20,3 +27,37 @@ class Movimentacao():
             self.player.x += self.speedRIGHT * self.janela.delta_time()
         if (Window.keyboard.key_pressed("a") or Window.keyboard.key_pressed("LEFT")) and (self.player.x+33 > 0):
             self.player.x -= self.speedLEFT * self.janela.delta_time()
+=======
+
+    def verify_colision(self, lista_objetos):
+        for objeto in lista_objetos:
+            if self.hitbox.collided(objeto):
+                return True
+        return False
+                    
+
+    def moviment(self, lista_objetos):
+        if (Window.keyboard.key_pressed("w") or Window.keyboard.key_pressed("UP")) and (self.hitbox.y+100 > 190):
+            self.hitbox.y -= self.speed * self.janela.delta_time()
+            if self.verify_colision(lista_objetos):
+                self.hitbox.y += self.speed * self.janela.delta_time()
+
+        if (Window.keyboard.key_pressed("s") or Window.keyboard.key_pressed("DOWN")) and (self.hitbox.y+100 < self.janela.height):
+            self.hitbox.y += self.speed * self.janela.delta_time()
+            if self.verify_colision(lista_objetos):
+                self.hitbox.y -= self.speed * self.janela.delta_time()
+
+        if (Window.keyboard.key_pressed("d") or Window.keyboard.key_pressed("RIGHT")) and (self.hitbox.x+60 < self.janela.width):
+            self.hitbox.x += self.speed * self.janela.delta_time()
+            if self.verify_colision(lista_objetos):
+                self.hitbox.x -= self.speed * self.janela.delta_time()
+
+        if (Window.keyboard.key_pressed("a") or Window.keyboard.key_pressed("LEFT")) and (self.hitbox.x+33 > 0):
+            self.hitbox.x -= self.speed * self.janela.delta_time()
+            if self.verify_colision(lista_objetos):
+                self.hitbox.x += self.speed * self.janela.delta_time()
+
+
+        self.player.x = self.hitbox.x - 8
+        self.player.y = self.hitbox.y - 135
+>>>>>>> Stashed changes
